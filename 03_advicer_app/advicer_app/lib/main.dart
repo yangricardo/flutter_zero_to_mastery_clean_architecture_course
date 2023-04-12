463dcc71a4a0bbfe55a3d5d947eb82a38345f89b
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-12 16:03:12 -0300

    feat: configure global theme service notifier

--- 03_advicer_app/advicer_app/lib/main.dart
+++ 03_advicer_app/advicer_app/lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:advicer_app/application/core/services/theme.service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeService(),
+    child: const AdvicerApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AdvicerApp extends StatelessWidget {
+  const AdvicerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Advicer APP',
+      themeMode: ThemeMode.light,
       home: Placeholder(),
     );
   }
