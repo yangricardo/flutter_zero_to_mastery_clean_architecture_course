950197fc7a232928e65e72cbcfa9cb64ac688d47
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-14 12:43:23 -0300

    test: fake random error

--- 03_advicer_app/advicer_app/lib/domain/usecases/advice.usecases.dart
+++ 03_advicer_app/advicer_app/lib/domain/usecases/advice.usecases.dart
@@ -7,8 +7,13 @@ import 'package:dartz/dartz.dart';
 class AdviceUseCases {
   Future<Either<AdviceEntity, Failure>> getAdvice() async {
     Random random = Random();
+    int randomId = random.nextInt(10);
     await Future.delayed(const Duration(seconds: 3), () {});
-    return left(AdviceEntity(
-        advice: 'Some fake advice from entity', id: random.nextInt(10)));
+    if (randomId % 2 == 0) {
+      return left(
+          AdviceEntity(advice: 'Some fake advice from entity', id: randomId));
+    }
+
+    return right(GeneralFailure());
   }
 }
