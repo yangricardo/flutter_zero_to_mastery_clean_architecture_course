9f8d4143eb3f5407bd33b89af7881ab266fb705a
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-14 11:36:32 -0300

    feat: cubit state management

---
+++ 03_advicer_app/advicer_app/lib/application/pages/advice/cubit/advicer_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+part 'advicer_state.dart';
+
+class AdvicerCubit extends Cubit<AdvicerCubitState> {
+  AdvicerCubit() : super(AdvicerInitial());
+
+  void adviceRequested() async {
+    emit(AdvicerCubitStateLoading());
+    // execute business logic
+    // like get and advice
+    debugPrint('fake get advices triggered');
+    await Future.delayed(const Duration(seconds: 3), () {});
+    debugPrint('got advice');
+    emit(const AdvicerCubitStateLoaded(advice: 'fake advice to test'));
+  }
+}
---
+++ 03_advicer_app/advicer_app/lib/application/pages/advice/cubit/advicer_state.dart
@@ -0,0 +1,30 @@
+part of 'advicer_cubit.dart';
+
+abstract class AdvicerCubitState extends Equatable {
+  const AdvicerCubitState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AdvicerInitial extends AdvicerCubitState {}
+
+class AdvicerCubitStateLoading extends AdvicerCubitState {}
+
+class AdvicerCubitStateLoaded extends AdvicerCubitState {
+  final String advice;
+
+  const AdvicerCubitStateLoaded({required this.advice});
+
+  @override
+  List<Object?> get props => [advice];
+}
+
+class AdvicerCubitStateError extends AdvicerCubitState {
+  final String message;
+
+  const AdvicerCubitStateError(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
