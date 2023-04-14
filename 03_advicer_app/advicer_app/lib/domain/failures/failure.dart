4b8a0ac2fd9ffb296a8cfe32af28bfc2d78c9d52
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-14 12:45:57 -0300

    feat: mapFailureToMessage

--- 03_advicer_app/advicer_app/lib/domain/failures/failure.dart
+++ 03_advicer_app/advicer_app/lib/domain/failures/failure.dart
@@ -5,3 +5,18 @@ class ServerFailure extends Failure {}
 class CacheFailure extends Failure {}
 
 class GeneralFailure extends Failure {}
+
+const generalFailureMessage = 'Ups, something gone wrong. Please try again!';
+const serverFailureMessage = 'Ups, API Error. please try again!';
+const cacheFailureMessage = 'Ups, cache failed. Please try again!';
+
+String mapFailureToMessage(Failure failure) {
+  switch (failure.runtimeType) {
+    case ServerFailure:
+      return serverFailureMessage;
+    case CacheFailure:
+      return cacheFailureMessage;
+    default:
+      return generalFailureMessage;
+  }
+}
