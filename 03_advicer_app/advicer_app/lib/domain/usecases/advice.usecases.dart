1eb15eb993e363e82cc8ccf83c4051d2ea91bd25
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-14 12:32:17 -0300

    fix: make getAdvice return advice or failure

--- 03_advicer_app/advicer_app/lib/domain/usecases/advice.usecases.dart
+++ 03_advicer_app/advicer_app/lib/domain/usecases/advice.usecases.dart
@@ -1,11 +1,14 @@
 import 'package:advicer_app/domain/entities/advice.entity.dart';
+import 'package:advicer_app/domain/failures/failure.dart';
 import 'dart:math';
 
+import 'package:dartz/dartz.dart';
+
 class AdviceUseCases {
-  Future<AdviceEntity> getAdvice() async {
+  Future<Either<AdviceEntity, Failure>> getAdvice() async {
     Random random = Random();
     await Future.delayed(const Duration(seconds: 3), () {});
-    return AdviceEntity(
-        advice: 'Some fake advice from entity', id: random.nextInt(10));
+    return left(AdviceEntity(
+        advice: 'Some fake advice from entity', id: random.nextInt(10)));
   }
 }
