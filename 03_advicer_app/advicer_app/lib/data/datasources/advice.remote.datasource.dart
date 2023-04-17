5c77e88af73fbdf89a96f85fa1a9d473bbe8cc39
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-17 12:54:29 -0300

    fix: dependency injection config

--- 03_advicer_app/advicer_app/lib/data/datasources/advice.remote.datasource.dart
+++ 03_advicer_app/advicer_app/lib/data/datasources/advice.remote.datasource.dart
@@ -12,7 +12,9 @@ abstract class AdviceRemoteDatasource {
 }
 
 class AdviceRemoteDatasourceImpl implements AdviceRemoteDatasource {
-  final client = http.Client();
+  final http.Client client;
+
+  AdviceRemoteDatasourceImpl({required this.client});
 
   @override
   Future<AdviceModel> getRandomAdviceFromApi() async {
--- 03_advicer_app/advicer_app/lib/domain/usecases/advice.usecases.dart
+++ 03_advicer_app/advicer_app/lib/domain/usecases/advice.usecases.dart
@@ -1,11 +1,11 @@
-import 'package:advicer_app/data/repositories/advice.repo.impl.dart';
 import 'package:advicer_app/domain/entities/advice.entity.dart';
 import 'package:advicer_app/domain/failures/failure.dart';
+import 'package:advicer_app/domain/repositories/advice.repo.dart';
 
 import 'package:dartz/dartz.dart';
 
 class AdviceUseCases {
-  final AdviceRepoImpl adviceRepo;
+  final AdviceRepo adviceRepo;
 
   AdviceUseCases({required this.adviceRepo});
 
