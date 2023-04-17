3676a0ef8046e6b2bc6ec2dd4a5631d0232f94c8
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-17 15:55:18 -0300

    test: when AdviceRepoImpl returns a AdviceModel

---
+++ 03_advicer_app/advicer_app/test/domain/AdviceUseCases_test.dart
@@ -0,0 +1,36 @@
+import 'package:advicer_app/data/repositories/advice.repo.impl.dart';
+import 'package:advicer_app/domain/entities/advice.entity.dart';
+import 'package:advicer_app/domain/failures/failure.dart';
+import 'package:advicer_app/domain/usecases/advice.usecases.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'AdviceUseCases_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<AdviceRepoImpl>()])
+void main() {
+  group('AdviceUseCases', () {
+    group('should return AdviceEntity', () {
+      test(
+          'when AdviceRepoImpl returns a AdviceModel',
+          () => () async {
+                final mockAdviceRepoImpl = MockAdviceRepoImpl();
+                final adviceUseCaseUnderTest =
+                    AdviceUseCases(adviceRepo: mockAdviceRepoImpl);
+                when(mockAdviceRepoImpl.getAdviceFromDataSource()).thenAnswer(
+                    (realInvocation) => Future.value(
+                        const Left(AdviceEntity(advice: 'test', id: 42))));
+                final result = await adviceUseCaseUnderTest.getAdvice();
+                expect(result.isLeft(), true);
+                expect(result.isRight(), false);
+                expect(
+                    result,
+                    const Left<AdviceEntity, Failure>(
+                        AdviceEntity(advice: 'test', id: 42)));
+                verify(mockAdviceRepoImpl.getAdviceFromDataSource()).called(1);
+                verifyNoMoreInteractions(mockAdviceRepoImpl);
+              });
+    });
+  });
+}
