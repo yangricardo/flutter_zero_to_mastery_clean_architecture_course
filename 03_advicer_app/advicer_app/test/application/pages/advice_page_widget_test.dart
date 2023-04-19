0a292c91eb63c8ee2acc354da9cdd5c58f4d7fda
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-19 14:08:04 -0300

    test: advice cubit initial state

---
+++ 03_advicer_app/advicer_app/test/application/pages/advice_page_widget_test.dart
@@ -0,0 +1,40 @@
+import 'package:advicer_app/application/core/services/theme.service.dart';
+import 'package:advicer_app/application/pages/advice/advice.page.dart';
+import 'package:advicer_app/application/pages/advice/cubit/advicer_cubit.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+class MockAdvicerCubit extends MockCubit<AdvicerCubitState>
+    implements AdvicerCubit {}
+
+void main() {
+  Widget widgetUnderTest({required AdvicerCubit cubit}) {
+    return MaterialApp(
+        home: ChangeNotifierProvider(
+      create: (context) => ThemeService(),
+      child: BlocProvider<AdvicerCubit>(
+        create: (context) => cubit,
+        child: const AdvicerPage(),
+      ),
+    ));
+  }
+
+  group('AdvicerPage', () {
+    late AdvicerCubit mockAdvicerCubit;
+    setUp(() => {mockAdvicerCubit = MockAdvicerCubit()});
+    group('should be displayed in ViewState', () {
+      testWidgets('Initial when cubit emits AdvicerInitial',
+          (widgetTester) async {
+        whenListen(mockAdvicerCubit, Stream.fromIterable([AdvicerInitial()]),
+            initialState: AdvicerInitial());
+        await widgetTester.pumpWidget(widgetUnderTest(cubit: mockAdvicerCubit));
+        final advicerInitialTextFinder =
+            find.text('Your advice is waiting for you!');
+        expect(advicerInitialTextFinder, findsOneWidget);
+      });
+    });
+  });
+}
