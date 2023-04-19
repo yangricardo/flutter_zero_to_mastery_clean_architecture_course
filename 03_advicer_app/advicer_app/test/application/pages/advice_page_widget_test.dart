b668ec760b6b75eed0d8ced728659cba91837c51
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-19 14:20:06 -0300

    test: Error when cubit emits AdvicerCubitStateError()

--- 03_advicer_app/advicer_app/test/application/pages/advice_page_widget_test.dart
+++ 03_advicer_app/advicer_app/test/application/pages/advice_page_widget_test.dart
@@ -2,6 +2,7 @@ import 'package:advicer_app/application/core/services/theme.service.dart';
 import 'package:advicer_app/application/pages/advice/advice.page.dart';
 import 'package:advicer_app/application/pages/advice/cubit/advicer_cubit.dart';
 import 'package:advicer_app/application/pages/advice/widgets/advice.field.dart';
+import 'package:advicer_app/application/pages/advice/widgets/error.message.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -63,6 +64,20 @@ void main() {
             widgetTester.widget<AdviceField>(advicerLoadedFinder).advice;
         expect(advicerLoadedFinder, findsOneWidget);
         expect(adviceText, adviceTextFinder);
+      });
+
+      testWidgets('Error when cubit emits AdvicerCubitStateError()',
+          (widgetTester) async {
+        const messageError = 'Advice sample';
+        whenListen(
+            mockAdvicerCubit,
+            Stream.fromIterable(
+                const [AdvicerCubitStateError(message: messageError)]),
+            initialState: AdvicerInitial());
+        await widgetTester.pumpWidget(widgetUnderTest(cubit: mockAdvicerCubit));
+        await widgetTester.pump();
+        final advicerLoadedFinder = find.byType(ErrorMessage);
+        expect(advicerLoadedFinder, findsOneWidget);
       });
     });
   });
