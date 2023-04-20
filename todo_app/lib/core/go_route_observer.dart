a66270b7cd92a266c246c4b3576417b6b05f526b
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-20 13:19:20 -0300

    feat: GoRouteObserver

---
+++ todo_app/lib/core/go_route_observer.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+class GoRouteObserver extends NavigatorObserver {}
--- todo_app/lib/core/routes.dart
+++ todo_app/lib/core/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import './go_route_observer.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
 
 final routes = GoRouter(
     navigatorKey: _rootNavigatorKey,
+    observers: [GoRouteObserver()],
     initialLocation: '/home/start',
     routes: [
       GoRoute(
