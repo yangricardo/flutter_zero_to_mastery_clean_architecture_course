7e8c4f88cb013944ab1fd04bddfd43f4ae566ba3
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-20 13:21:17 -0300

    feat: GoRoute Observer methods

--- todo_app/lib/core/go_route_observer.dart
+++ todo_app/lib/core/go_route_observer.dart
@@ -1,3 +1,39 @@
 import 'package:flutter/material.dart';
 
-class GoRouteObserver extends NavigatorObserver {}
+class GoRouteObserver extends NavigatorObserver {
+  GoRouteObserver();
+
+  @override
+  void didPop(Route route, Route? previousRoute) {
+    debugPrint('didPop: ${route.settings.name}');
+  }
+
+  @override
+  void didPush(Route route, Route? previousRoute) {
+    debugPrint(
+        'didPush: ${route.settings.name} previousRoute: ${previousRoute?.settings.name}');
+  }
+
+  @override
+  void didRemove(Route route, Route? previousRoute) {
+    debugPrint(
+        'didRemove: ${route.settings.name} previousRoute: ${previousRoute?.settings.name}');
+  }
+
+  @override
+  void didReplace({Route? newRoute, Route? oldRoute}) {
+    debugPrint(
+        'didReplace: ${newRoute?.settings.name} oldRoute: ${oldRoute?.settings.name}');
+  }
+
+  @override
+  void didStartUserGesture(Route route, Route? previousRoute) {
+    debugPrint(
+        'didStartUserGesture: ${route.settings.name} previousRoute: ${previousRoute?.settings.name}');
+  }
+
+  @override
+  void didStopUserGesture() {
+    debugPrint('didStopUserGesture');
+  }
+}
