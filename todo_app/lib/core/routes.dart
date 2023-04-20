9f31ef659b5a3ce8a9bedd2223950b86f835ed90
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-20 13:15:35 -0300

    feat: root navigator key

--- todo_app/lib/core/routes.dart
+++ todo_app/lib/core/routes.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-final routes = GoRouter(initialLocation: '/home/start', routes: [
-  GoRoute(
-    path: '/home/settings',
-    builder: (context, state) {
-      return Container(color: Colors.amber);
-    },
-  ),
-  GoRoute(
-    path: '/home/start',
-    builder: (context, state) {
-      return Container(color: Colors.blue);
-    },
-  )
-]);
+final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+
+final routes = GoRouter(
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: '/home/start',
+    routes: [
+      GoRoute(
+        path: '/home/settings',
+        builder: (context, state) {
+          return Container(color: Colors.amber);
+        },
+      ),
+      GoRoute(
+        path: '/home/start',
+        builder: (context, state) {
+          return Container(color: Colors.blue);
+        },
+      )
+    ]);
