84d14fdf126e7293b5b590b9e2612ba9787b44c7
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-20 15:20:58 -0300

    feat(HomePage): add tab options

--- todo_app/lib/pages/home/home_page.dart
+++ todo_app/lib/pages/home/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/pages/dashboard/dashboard_page.dart';
+import 'package:todo_app/pages/overview/overview_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  static const tabs = [DashboardPage.pageConfig, OverviewPage.pageConfig];
 
   @override
   State<StatefulWidget> createState() => _HomePageState();
