a210cdcd26e327397a2c6cce6c9cc578241f7641
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-24 13:52:03 -0300

    feat: abstract Params class and  NoParams

--- todo_app/lib/core/use_case.dart
+++ todo_app/lib/core/use_case.dart
@@ -1,6 +1,17 @@
 import 'package:either_dart/either.dart';
+import 'package:equatable/equatable.dart';
 import 'package:todo_app/domain/failures/failure.dart';
+
+abstract class Params extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams extends Params {
+  @override
+  List<Object?> get props => [];
 }
