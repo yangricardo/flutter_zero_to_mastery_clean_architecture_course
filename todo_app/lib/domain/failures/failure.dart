420738cbf6f0155cc3e764b7f09a29b1e02f71f6
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-24 14:22:32 -0300

    feat: server failure wrapper

--- todo_app/lib/domain/failures/failure.dart
+++ todo_app/lib/domain/failures/failure.dart
@@ -1,1 +1,12 @@
+import 'package:equatable/equatable.dart';
+
 abstract class Failure {}
+
+class ServerFailure extends Failure with EquatableMixin {
+  final String? stackTrace;
+
+  ServerFailure({this.stackTrace});
+
+  @override
+  List<Object?> get props => [stackTrace];
+}
