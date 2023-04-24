1d7d77c64d076085177fdc0f71d8d71665c7174c
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-24 14:58:01 -0300

    test: generate load todo collections mocks
    flutter pub run build_runner build -d

---
+++ todo_app/test/domain/use_cases/load_todo_collection_test.dart
@@ -0,0 +1,8 @@
+import 'package:mockito/annotations.dart';
+import 'package:test/test.dart';
+import 'package:todo_app/domain/use_cases/load_todo_collection.dart';
+
+@GenerateNiceMocks([MockSpec<LoadToDoCollections>()])
+void main() {
+  group('LoadTodoCollection', () {});
+}
