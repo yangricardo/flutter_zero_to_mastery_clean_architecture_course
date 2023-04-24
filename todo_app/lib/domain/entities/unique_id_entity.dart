b6e3224b7130476de92f90a9cd75b9bc90da7c87
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-24 13:30:49 -0300

    feat: unique id and collection id

---
+++ todo_app/lib/domain/entities/unique_id_entity.dart
@@ -0,0 +1,26 @@
+import 'package:uuid/uuid.dart';
+
+class UniqueId {
+  const UniqueId._(this.value);
+  final String value;
+
+  factory UniqueId() {
+    return UniqueId._(const Uuid().v4());
+  }
+
+  factory UniqueId.fromUniqueString(String uniqueId) {
+    return UniqueId._(uniqueId);
+  }
+}
+
+class CollectionId extends UniqueId {
+  const CollectionId._(String value) : super._(value);
+
+  factory CollectionId() {
+    return CollectionId._(const Uuid().v4());
+  }
+
+  factory CollectionId.fromUniqueString(String uniqueId) {
+    return CollectionId._(uniqueId);
+  }
+}
