a00e4e3742bae5dc13d87f05fde9324a75b35670
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-25 12:12:41 -0300

    feat(ToDoOverviewCubit): define constructor initial state

--- todo_app/lib/pages/overview/cubit/todo_overview_cubit.dart
+++ todo_app/lib/pages/overview/cubit/todo_overview_cubit.dart
@@ -1,8 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_app/domain/entities/todo_collection_entity.dart';
+import 'package:todo_app/domain/use_cases/load_todo_collection.dart';
 
 part 'todo_overview_state.dart';
 
 class TodoOverviewCubit extends Cubit<TodoOverviewState> {
-  TodoOverviewCubit() : super(TodoOverviewInitial());
+  final LoadToDoCollections loadToDoCollections;
+  TodoOverviewCubit({
+    required this.loadToDoCollections,
+    TodoOverviewState? initialState,
+  }) : super(initialState ?? ToDoOverviewLoadingState());
 }
