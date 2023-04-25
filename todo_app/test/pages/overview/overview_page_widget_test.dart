248ed87d06dbd831033c514a399e49056f57238d
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-25 14:05:19 -0300

    test(OverviewPage): loaded state should display list view

--- todo_app/test/pages/overview/overview_page_widget_test.dart
+++ todo_app/test/pages/overview/overview_page_widget_test.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:todo_app/data/repositories/todo_repository_mock.dart';
+import 'package:todo_app/domain/entities/todo_collection_entity.dart';
+import 'package:todo_app/domain/entities/todo_color_entity.dart';
+import 'package:todo_app/domain/entities/unique_id_entity.dart';
 import 'package:todo_app/domain/repositories/todo_repository.dart';
 import 'package:todo_app/pages/overview/cubit/todo_overview_cubit.dart';
 import 'package:todo_app/pages/overview/overview_page.dart';
+import 'package:todo_app/pages/overview/view_states/todo_overview_loaded.dart';
 
 class MockOverviewPageCubit extends MockCubit<TodoOverviewState>
     implements TodoOverviewCubit {}
@@ -13,12 +17,13 @@ class MockOverviewPageCubit extends MockCubit<TodoOverviewState>
 void main() {
   Widget widgetUnderTest({required TodoOverviewCubit cubit}) {
     return MaterialApp(
-        home: RepositoryProvider<TodoRepository>(
-            create: (context) => TodoRepositoryMock(),
-            child: BlocProvider(
-              create: (context) => cubit,
-              child: const OverviewPage(),
-            )));
+        home: Material(
+            child: RepositoryProvider<TodoRepository>(
+                create: (context) => TodoRepositoryMock(),
+                child: BlocProvider(
+                  create: (context) => cubit,
+                  child: const OverviewPage(),
+                ))));
   }
 
   group('OverviewPage', () {
@@ -38,6 +43,31 @@ void main() {
               .pumpWidget(widgetUnderTest(cubit: mockTodoOverviewCubit));
           await widgetTester.pump();
           expect(find.byType(CircularProgressIndicator), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'loaded state should display list view',
+        (widgetTester) async {
+          final todoCollections = List<ToDoCollection>.generate(
+              10,
+              (index) => ToDoCollection(
+                  id: CollectionId.fromUniqueString(index.toString()),
+                  title: 'Task $index',
+                  color: ToDoColor(
+                      colorIndex: index % ToDoColor.predefinedColors.length)));
+          whenListen(
+            mockTodoOverviewCubit,
+            Stream.fromIterable(
+                [ToDoOverviewLoadedState(collections: todoCollections)]),
+            initialState: ToDoOverviewLoadingState(),
+          );
+          await widgetTester
+              .pumpWidget(widgetUnderTest(cubit: mockTodoOverviewCubit));
+          await widgetTester.pumpAndSettle();
+          expect(find.byType(ToDoOverviewLoaded), findsOneWidget);
+          expect(find.byType(ListView), findsOneWidget);
+          expect(find.byType(ListTile), findsNWidgets(10));
         },
       );
     });
