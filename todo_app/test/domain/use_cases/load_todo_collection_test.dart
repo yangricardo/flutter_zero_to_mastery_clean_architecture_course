c6228cf875cde59013de558baf4df26942dd460f
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-25 11:52:43 -0300

    test: expect LoadTodoCollection returns 10 loadedCollections

--- todo_app/test/domain/use_cases/load_todo_collection_test.dart
+++ todo_app/test/domain/use_cases/load_todo_collection_test.dart
@@ -1,8 +1,19 @@
-import 'package:mockito/annotations.dart';
 import 'package:test/test.dart';
+import 'package:todo_app/core/use_case.dart';
+import 'package:todo_app/data/repositories/todo_repository_mock.dart';
 import 'package:todo_app/domain/use_cases/load_todo_collection.dart';
 
-@GenerateNiceMocks([MockSpec<LoadToDoCollections>()])
 void main() {
-  group('LoadTodoCollection', () {});
+  group('LoadTodoCollection', () {
+    group('should return ToDoCollection', () {
+      test('expect LoadTodoCollection returns 10 loadedCollections', () async {
+        final mockLoadToDoCollections =
+            LoadToDoCollections(todoRepository: TodoRepositoryMock());
+        final result = await mockLoadToDoCollections.call(NoParams());
+        expect(result.isLeft, false);
+        expect(result.isRight, true);
+        expect(result.right.length, 10);
+      });
+    });
+  });
 }
