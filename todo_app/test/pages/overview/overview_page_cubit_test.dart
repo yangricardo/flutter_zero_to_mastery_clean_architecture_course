fc10807b479a5222eb97e05b9be4481df06bdd73
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-25 13:04:57 -0300

    test(ToDoOverviewCubit): error state when readToDoCollections is called and server failure occurs

--- todo_app/test/pages/overview/overview_page_cubit_test.dart
+++ todo_app/test/pages/overview/overview_page_cubit_test.dart
@@ -6,6 +6,7 @@ import 'package:todo_app/core/use_case.dart';
 import 'package:todo_app/domain/entities/todo_collection_entity.dart';
 import 'package:todo_app/domain/entities/todo_color_entity.dart';
 import 'package:todo_app/domain/entities/unique_id_entity.dart';
+import 'package:todo_app/domain/failures/failure.dart';
 import 'package:todo_app/domain/use_cases/load_todo_collection.dart';
 import 'package:todo_app/pages/overview/cubit/todo_overview_cubit.dart';
 
@@ -41,6 +42,25 @@ void main() {
                   expect: () => <TodoOverviewState>[
                         ToDoOverviewLoadingState(),
                         ToDoOverviewLoadedState(collections: todoCollections)
+                      ]);
+
+              blocTest('error state when readToDoCollections is called',
+                  setUp: () => {
+                        when(() => mockLoadToDoCollections.call(NoParams()))
+                            .thenAnswer((_) async =>
+                                Future.value(Left(ServerFailure())))
+                      },
+                  build: () {
+                    when(() => mockLoadToDoCollections.call(NoParams()))
+                        .thenAnswer(
+                            (_) async => Future.value(Left(ServerFailure())));
+                    return TodoOverviewCubit(
+                        loadToDoCollections: mockLoadToDoCollections);
+                  },
+                  act: (cubit) => cubit.readToDoCollections(),
+                  expect: () => <TodoOverviewState>[
+                        ToDoOverviewLoadingState(),
+                        ToDoOverviewErrorState()
                       ]);
             })
           });
