1e29cb44cbadef9374fca21a317cfe375cacf868
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-25 12:34:25 -0300

    fix: repository provider definition

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/app/basic_app.dart';
+import 'package:todo_app/data/repositories/todo_repository_mock.dart';
+import 'package:todo_app/domain/repositories/todo_repository.dart';
 
 void main() {
-  runApp(const BasicApp());
+  runApp(RepositoryProvider<TodoRepository>(
+    create: (context) => TodoRepositoryMock(),
+    child: const BasicApp(),
+  ));
 }
