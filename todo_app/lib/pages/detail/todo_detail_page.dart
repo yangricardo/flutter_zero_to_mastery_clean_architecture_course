b1e424f1d6907db15593fe7db1ee89e3decccaca
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-25 16:17:38 -0300

    feat(ToDoDetailPage): render page conforming cubit state

--- todo_app/lib/pages/detail/todo_detail_page.dart
+++ todo_app/lib/pages/detail/todo_detail_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/core/page_config.dart';
 import 'package:todo_app/domain/entities/unique_id_entity.dart';
+import 'package:todo_app/domain/repositories/todo_repository.dart';
+import 'package:todo_app/domain/use_cases/load_todo_entry_ids_from_collection.dart';
+import 'package:todo_app/pages/detail/cubit/to_do_detail_cubit.dart';
+import 'package:todo_app/pages/detail/view_states/todo_detail_error.dart';
+import 'package:todo_app/pages/detail/view_states/todo_detail_loaded.dart';
+import 'package:todo_app/pages/detail/view_states/todo_detail_loading.dart';
 
 class ToDoDetailPageProvider extends StatelessWidget {
   const ToDoDetailPageProvider({
@@ -12,20 +19,49 @@ class ToDoDetailPageProvider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ToDoDetailPage();
+    return BlocProvider<ToDoDetailCubit>(
+      create: (context) => ToDoDetailCubit(
+        collectionId: collectionId,
+        loadToDoEntryIdsForCollection: LoadToDoEntryIdsFromCollection(
+          toDoRepository: RepositoryProvider.of<TodoRepository>(context),
+        ),
+      )..fetch(),
+      child: ToDoDetailPage(
+        collectionId: collectionId,
+      ),
+    );
   }
 }
 
 class ToDoDetailPage extends StatelessWidget {
-  const ToDoDetailPage({super.key});
+  const ToDoDetailPage({
+    super.key,
+    required this.collectionId,
+  });
+
+  final CollectionId collectionId;
 
   static const pageConfig = PageConfig(
-    icon: Icons.work_history_rounded,
+    icon: Icons.details_rounded,
     name: 'detail',
+    child: Placeholder(),
   );
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocBuilder<ToDoDetailCubit, ToDoDetailState>(
+      builder: (context, state) {
+        if (state is ToDoDetailCubitLoadingState) {
+          return const ToDoDetailLoading();
+        } else if (state is ToDoDetailCubitLoadedState) {
+          return ToDoDetailLoaded(
+            collectionId: collectionId,
+            entryIds: state.entryIds,
+          );
+        } else {
+          return const ToDoDetailError();
+        }
+      },
+    );
   }
 }
