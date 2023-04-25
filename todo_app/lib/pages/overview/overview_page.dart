00b5c8e64907446c95cbf75c8247c07cc1059d2a
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-25 12:20:14 -0300

    feat: OverviewPageProvider

--- todo_app/lib/pages/overview/overview_page.dart
+++ todo_app/lib/pages/overview/overview_page.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/core/page_config.dart';
+import 'package:todo_app/domain/use_cases/load_todo_collection.dart';
+import 'package:todo_app/pages/overview/cubit/todo_overview_cubit.dart';
+
+class OverviewPageProvider extends StatelessWidget {
+  const OverviewPageProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TodoOverviewCubit(
+          loadToDoCollections: LoadToDoCollections(
+              todoRepository: RepositoryProvider.of(context))),
+      child: const OverviewPage(),
+    );
+  }
+}
 
 class OverviewPage extends StatelessWidget {
   const OverviewPage({super.key});
