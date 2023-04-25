bf44c28d3b6204c4c7f3ed71fc02ff516990dc09
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-25 12:15:51 -0300

    feat(ToDoOverviewCubit): define read todo collection

--- todo_app/lib/pages/overview/cubit/todo_overview_cubit.dart
+++ todo_app/lib/pages/overview/cubit/todo_overview_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_app/core/use_case.dart';
 import 'package:todo_app/domain/entities/todo_collection_entity.dart';
 import 'package:todo_app/domain/use_cases/load_todo_collection.dart';
 
@@ -11,4 +12,18 @@ class TodoOverviewCubit extends Cubit<TodoOverviewState> {
     required this.loadToDoCollections,
     TodoOverviewState? initialState,
   }) : super(initialState ?? ToDoOverviewLoadingState());
+
+  Future<void> readToDoCollections() async {
+    emit(ToDoOverviewLoadingState());
+    try {
+      final collections = await loadToDoCollections.call(NoParams());
+      if (collections.isLeft) {
+        emit(ToDoOverviewErrorState());
+      } else {
+        emit(ToDoOverviewLoadedState(collections: collections.right));
+      }
+    } on Exception {
+      emit(ToDoOverviewErrorState());
+    }
+  }
 }
