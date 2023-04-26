6a1646f291bd0c2ab1eb9bb52666d0431ec6b3fd
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-26 13:21:53 -0300

    feat: CreateTodoCollectionPageProvider

--- todo_app/lib/pages/create_todo_collection/create_todo_collection_page.dart
+++ todo_app/lib/pages/create_todo_collection/create_todo_collection_page.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/core/page_config.dart';
+import 'package:todo_app/pages/create_todo_collection/cubit/create_todo_collection_page_cubit.dart';
+
+class CreateTodoCollectionPageProvider extends StatelessWidget {
+  const CreateTodoCollectionPageProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CreateTodoCollectionPageCubit(),
+      child: const CreateTodoCollectionPage(),
+    );
+  }
+}
 
 class CreateTodoCollectionPage extends StatefulWidget {
   const CreateTodoCollectionPage({super.key});
