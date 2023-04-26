0aeeaefa4b1a94b5af955f5b25e4bfb217f795a9
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-26 10:57:58 -0300

    feat: ToDoEntryItemProvider with loadToDoEntry

--- todo_app/lib/components/todo_entry_item/todo_entry_item.dart
+++ todo_app/lib/components/todo_entry_item/todo_entry_item.dart
@@ -1,4 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/components/todo_entry_item/cubit/todo_entry_item_cubit.dart';
+import 'package:todo_app/domain/entities/unique_id_entity.dart';
+import 'package:todo_app/domain/repositories/todo_repository.dart';
+import 'package:todo_app/domain/use_cases/load_todo_entry.dart';
+
+class ToDoEntryItemProvider extends StatelessWidget {
+  final EntryId entryId;
+  final CollectionId collectionId;
+  const ToDoEntryItemProvider(
+      {super.key, required this.entryId, required this.collectionId});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ToDoEntryItemCubit(
+        entryId: entryId,
+        collectionId: collectionId,
+        loadToDoEntry: LoadToDoEntry(
+            todoRepository: RepositoryProvider.of<TodoRepository>(context)),
+      )..fetch(),
+      child: const ToDoEntryItem(),
+    );
+  }
+}
 
 class ToDoEntryItem extends StatelessWidget {
   const ToDoEntryItem({super.key});
