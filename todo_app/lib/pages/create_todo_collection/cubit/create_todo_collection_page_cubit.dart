b79e9162aa9d0930de1255cf20cd0411623e32d7
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-26 14:05:19 -0300

    feat(CreateTodoCollectionPageCubit): submit

--- todo_app/lib/pages/create_todo_collection/cubit/create_todo_collection_page_cubit.dart
+++ todo_app/lib/pages/create_todo_collection/cubit/create_todo_collection_page_cubit.dart
@@ -1,12 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_app/core/use_case.dart';
+import 'package:todo_app/domain/entities/todo_collection_entity.dart';
+import 'package:todo_app/domain/entities/todo_color_entity.dart';
+import 'package:todo_app/domain/use_cases/create_todo_collection.dart';
 
 part 'create_todo_collection_page_state.dart';
 
 class CreateTodoCollectionPageCubit
     extends Cubit<CreateTodoCollectionPageState> {
-  CreateTodoCollectionPageCubit()
-      : super(const CreateTodoCollectionPageState());
+  final CreateTodoCollection createToDoCollection;
+
+  CreateTodoCollectionPageCubit({
+    required this.createToDoCollection,
+  }) : super(const CreateTodoCollectionPageState());
 
   void titleChanged(String? title) {
     emit(state.copyWith(title: title));
@@ -14,5 +21,16 @@ class CreateTodoCollectionPageCubit
 
   void colorChanged(String? color) {
     emit(state.copyWith(color: color));
+  }
+
+  Future<void> submit() async {
+    final parsedColorIndex = int.tryParse(state.color ?? '') ?? 0;
+
+    await createToDoCollection.call(ToDoCollectionParams(
+      collection: ToDoCollection.empty().copyWith(
+        title: state.title,
+        color: ToDoColor(colorIndex: parsedColorIndex),
+      ),
+    ));
   }
 }
