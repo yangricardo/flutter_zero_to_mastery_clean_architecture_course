344da9bcee1f1a3264348613b2781d032ae28bc0
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-26 12:07:35 -0300

    fix(ToDoOverviewLoaded): on tap adds selected todo collection changed

--- todo_app/lib/pages/overview/view_states/todo_overview_loaded.dart
+++ todo_app/lib/pages/overview/view_states/todo_overview_loaded.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:todo_app/domain/entities/todo_collection_entity.dart';
 import 'package:todo_app/pages/detail/todo_detail_page.dart';
+import 'package:todo_app/pages/home/cubit/navigation_todo_cubit.dart';
 
 class ToDoOverviewLoaded extends StatelessWidget {
   final List<ToDoCollection> collections;
@@ -21,6 +23,9 @@ class ToDoOverviewLoaded extends StatelessWidget {
           iconColor: item.color.color,
           selectedColor: item.color.color,
           onTap: () {
+            context
+                .read<NavigationTodoCubit>()
+                .selectedToDoCollectionChanged(item.id);
             if (Breakpoints.small.isActive(context)) {
               context.pushNamed(
                 ToDoDetailPage.pageConfig.name,
