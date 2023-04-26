2e8ad49df322e1b24a013c02ceee3e8739025a20
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-26 10:44:29 -0300

    feat: todo entry item widget

---
+++ todo_app/lib/components/todo_entry_item/todo_entry_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ToDoEntryItem extends StatelessWidget {
+  const ToDoEntryItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
