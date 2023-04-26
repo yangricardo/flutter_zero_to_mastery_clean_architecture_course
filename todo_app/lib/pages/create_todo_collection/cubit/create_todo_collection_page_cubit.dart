7a1b608bfb65ed1a411203baf521e26f372ef7d2
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-26 13:49:29 -0300

    feat(CreateTodoCollectionPageCubit): title changed

--- todo_app/lib/pages/create_todo_collection/cubit/create_todo_collection_page_cubit.dart
+++ todo_app/lib/pages/create_todo_collection/cubit/create_todo_collection_page_cubit.dart
@@ -3,6 +3,12 @@ import 'package:equatable/equatable.dart';
 
 part 'create_todo_collection_page_state.dart';
 
-class CreateTodoCollectionPageCubit extends Cubit<CreateTodoCollectionPageState> {
-  CreateTodoCollectionPageCubit() : super(CreateTodoCollectionPageInitial());
+class CreateTodoCollectionPageCubit
+    extends Cubit<CreateTodoCollectionPageState> {
+  CreateTodoCollectionPageCubit()
+      : super(const CreateTodoCollectionPageState());
+
+  void titleChanged(String? title) {
+    emit(state.copyWith(title: title));
+  }
 }
