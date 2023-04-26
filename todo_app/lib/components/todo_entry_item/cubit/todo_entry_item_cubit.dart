901d5f221eed55d898cc97647cd8a8d21884327d
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-26 10:47:43 -0300

    feat(ToDoEntryItemCubit): add parameters and constructor

--- todo_app/lib/components/todo_entry_item/cubit/todo_entry_item_cubit.dart
+++ todo_app/lib/components/todo_entry_item/cubit/todo_entry_item_cubit.dart
@@ -1,9 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:todo_app/domain/entities/todo_entity.dart';
+import 'package:todo_app/domain/entities/unique_id_entity.dart';
+import 'package:todo_app/domain/use_cases/load_todo_entry.dart';
 
 part 'todo_entry_item_state.dart';
 
 class ToDoEntryItemCubit extends Cubit<ToDoEntryItemState> {
-  ToDoEntryItemCubit() : super(ToDoEntryItemLoadingState());
+  final EntryId entryId;
+  final CollectionId collectionId;
+  final LoadToDoEntry loadToDoEntry;
+  ToDoEntryItemCubit(
+      {required this.entryId,
+      required this.collectionId,
+      required this.loadToDoEntry})
+      : super(ToDoEntryItemLoadingState());
 }
