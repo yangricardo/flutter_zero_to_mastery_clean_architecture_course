41bfd4fca3beac3fceb688972e014c8cb1d0abe2
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-26 13:49:03 -0300

    feat(CreateTodoCollectionPageState): copyWith

--- todo_app/lib/pages/create_todo_collection/cubit/create_todo_collection_page_state.dart
+++ todo_app/lib/pages/create_todo_collection/cubit/create_todo_collection_page_state.dart
@@ -1,10 +1,18 @@
 part of 'create_todo_collection_page_cubit.dart';
 
-abstract class CreateTodoCollectionPageState extends Equatable {
-  const CreateTodoCollectionPageState();
+class CreateTodoCollectionPageState extends Equatable {
+  final String? title;
+  final String? color;
+
+  const CreateTodoCollectionPageState({this.title, this.color});
+
+  CreateTodoCollectionPageState copyWith({String? title, String? color}) {
+    return CreateTodoCollectionPageState(
+      color: color ?? this.color,
+      title: title ?? this.title,
+    );
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [title, color];
 }
-
-class CreateTodoCollectionPageInitial extends CreateTodoCollectionPageState {}
