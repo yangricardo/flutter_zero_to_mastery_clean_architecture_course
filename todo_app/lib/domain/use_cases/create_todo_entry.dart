c961106b50e181af97371f1bce4f19ab0404161b
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 12:44:35 -0300

    feat: CreateTodoEntry

---
+++ todo_app/lib/domain/use_cases/create_todo_entry.dart
@@ -0,0 +1,20 @@
+import 'package:either_dart/either.dart';
+import 'package:todo_app/core/use_case.dart';
+import 'package:todo_app/domain/failures/failure.dart';
+import 'package:todo_app/domain/repositories/todo_repository.dart';
+
+class CreateTodoEntry implements UseCase<bool, ToDoEntryParams> {
+  final TodoRepository todoRepository;
+
+  CreateTodoEntry({required this.todoRepository});
+
+  @override
+  Future<Either<Failure, bool>> call(ToDoEntryParams params) {
+    try {
+      final result = todoRepository.createToDoEntry(params.entry);
+      return result.fold((left) => Left(left), (right) => Right(right));
+    } on Exception catch (e) {
+      return Future.value(Left(ServerFailure(stackTrace: e.toString())));
+    }
+  }
+}
