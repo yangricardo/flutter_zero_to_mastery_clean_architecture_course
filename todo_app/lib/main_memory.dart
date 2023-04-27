4f31b136f3246267ab16658006663bd0066888d3
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 20:25:26 -0300

    refactor(main): use hive local database

--- todo_app/lib/main_memory.dart
+++ todo_app/lib/main_memory.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/app/basic_app.dart';
-import 'package:todo_app/data/data_sources/local/hive_local_data_source.dart';
+import 'package:todo_app/data/data_sources/local/memory_local_data_source.dart';
 import 'package:todo_app/data/repositories/todo_repository_local.dart';
 import 'package:todo_app/domain/repositories/todo_repository.dart';
 
-void main() async {
-  final localDataSource = HiveLocalDataSource();
-  await localDataSource.init();
+void main() {
   runApp(RepositoryProvider<TodoRepository>(
     create: (context) => LocalToDoRepository(
-      localDataSource: localDataSource,
+      localDataSource: MemoryLocalDataSource(),
     ),
     child: const BasicApp(),
   ));
