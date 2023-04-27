8a205ef41f5c4e8736ff5b8b8e98ac87f90c756e
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 11:16:12 -0300

    feat(CreateTodoEntryPageState): copy with description

--- todo_app/lib/pages/create_todo_entry/cubit/create_todo_entry_page_state.dart
+++ todo_app/lib/pages/create_todo_entry/cubit/create_todo_entry_page_state.dart
@@ -1,10 +1,16 @@
 part of 'create_todo_entry_page_cubit.dart';
 
-abstract class CreateTodoEntryPageState extends Equatable {
-  const CreateTodoEntryPageState();
+class CreateTodoEntryPageState extends Equatable {
+  final String? description;
+
+  const CreateTodoEntryPageState({this.description});
+
+  CreateTodoEntryPageState copyWith({String? description}) {
+    return CreateTodoEntryPageState(
+      description: description ?? this.description,
+    );
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [description];
 }
-
-class CreateTodoEntryPageInitial extends CreateTodoEntryPageState {}
