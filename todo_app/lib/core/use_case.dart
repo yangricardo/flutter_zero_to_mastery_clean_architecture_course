1a0aa9e0dd6297d1ef29aaa3dbbb132f9aa846e0
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 12:41:19 -0300

    feat(use_case): ToDoEntryParams

--- todo_app/lib/core/use_case.dart
+++ todo_app/lib/core/use_case.dart
@@ -1,6 +1,7 @@
 import 'package:either_dart/either.dart';
 import 'package:equatable/equatable.dart';
 import 'package:todo_app/domain/entities/todo_collection_entity.dart';
+import 'package:todo_app/domain/entities/todo_entity.dart';
 import 'package:todo_app/domain/entities/unique_id_entity.dart';
 import 'package:todo_app/domain/failures/failure.dart';
 
@@ -51,4 +52,15 @@ class ToDoCollectionParams extends Params {
 
   @override
   List<Object> get props => [collection];
+}
+
+class ToDoEntryParams extends Params {
+  ToDoEntryParams({
+    required this.entry,
+  }) : super();
+
+  final ToDoEntry entry;
+
+  @override
+  List<Object> get props => [entry];
 }
