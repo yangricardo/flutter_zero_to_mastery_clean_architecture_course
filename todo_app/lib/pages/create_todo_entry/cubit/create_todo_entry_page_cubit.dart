3114b7b472a72c41dab7c06dd707aea74a1ef421
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 11:13:36 -0300

    feat: create todo entry page cubit

---
+++ todo_app/lib/pages/create_todo_entry/cubit/create_todo_entry_page_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'create_todo_entry_page_state.dart';
+
+class CreateTodoEntryPageCubit extends Cubit<CreateTodoEntryPageState> {
+  CreateTodoEntryPageCubit() : super(CreateTodoEntryPageInitial());
+}
---
+++ todo_app/lib/pages/create_todo_entry/cubit/create_todo_entry_page_state.dart
@@ -0,0 +1,10 @@
+part of 'create_todo_entry_page_cubit.dart';
+
+abstract class CreateTodoEntryPageState extends Equatable {
+  const CreateTodoEntryPageState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CreateTodoEntryPageInitial extends CreateTodoEntryPageState {}
