d4a28b7ac5dfd43a045739f62fbc3b00d55cea00
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 14:46:45 -0300

    fix(ToDoLocalDataSourceInterface): update to new serialized models

---
+++ todo_app/lib/data/data_sources/interfaces/todo_local_data_source_interface.dart
@@ -0,0 +1,28 @@
+import 'package:todo_app/data/data_sources/models/todo_collection_model.dart';
+import 'package:todo_app/data/data_sources/models/todo_entry_model.dart';
+
+abstract class ToDoLocalDataSourceInterface {
+  Future<List<ToDoCollectionModel>> getToDoCollections();
+  Future<ToDoCollectionModel> getToDoCollection({
+    required String collectionId,
+  });
+
+  Future<List<String>> getToDoEntryIds({
+    required String collectionId,
+  });
+  Future<ToDoEntryModel> getToDoEntry({
+    required String collectionId,
+    required String entryId,
+  });
+  Future<bool> createToDoEntry({
+    required String collectionId,
+    required ToDoEntryModel entry,
+  });
+  Future<bool> createToDoCollection({
+    required ToDoCollectionModel collection,
+  });
+  Future<ToDoEntryModel> updateToDoEntry({
+    required String collectionId,
+    required String entryId,
+  });
+}
