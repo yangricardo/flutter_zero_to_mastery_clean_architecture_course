279fa45b05a60242b22109d797f53f8a065e124d
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 14:50:53 -0300

    feat(MemoryLocalDataSource): base implementation

---
+++ todo_app/lib/data/data_sources/local/memory_local_data_source.dart
@@ -0,0 +1,54 @@
+import 'package:todo_app/data/data_sources/interfaces/todo_local_data_source_interface.dart';
+import 'package:todo_app/data/data_sources/models/todo_entry_model.dart';
+import 'package:todo_app/data/data_sources/models/todo_collection_model.dart';
+
+class MemoryLocalDataSource implements ToDoLocalDataSourceInterface {
+  final List<ToDoCollectionModel> todoCollections = [];
+  final Map<String, List<ToDoEntryModel>> todoEntries = {};
+
+  @override
+  Future<bool> createToDoCollection({required ToDoCollectionModel collection}) {
+    // TODO: implement createToDoCollection
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> createToDoEntry(
+      {required String collectionId, required ToDoEntryModel entry}) {
+    // TODO: implement createToDoEntry
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ToDoCollectionModel> getToDoCollection(
+      {required String collectionId}) {
+    // TODO: implement getToDoCollection
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ToDoCollectionModel>> getToDoCollections() {
+    // TODO: implement getToDoCollections
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ToDoEntryModel> getToDoEntry(
+      {required String collectionId, required String entryId}) {
+    // TODO: implement getToDoEntry
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<String>> getToDoEntryIds({required String collectionId}) {
+    // TODO: implement getToDoEntryIds
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ToDoEntryModel> updateToDoEntry(
+      {required String collectionId, required String entryId}) {
+    // TODO: implement updateToDoEntry
+    throw UnimplementedError();
+  }
+}
