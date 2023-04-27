5d1ec1870fbcdffde1799b42ecd2022fa48c9521
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 17:33:27 -0300

    feat(HiveLocalDataSouce): getToDoEntry

--- todo_app/lib/data/data_sources/local/hive_local_data_source.dart
+++ todo_app/lib/data/data_sources/local/hive_local_data_source.dart
@@ -84,9 +84,16 @@ class HiveLocalDataSource implements ToDoLocalDataSourceInterface {
 
   @override
   Future<ToDoEntryModel> getToDoEntry(
-      {required String collectionId, required String entryId}) {
-    // TODO: implement getToDoEntry
-    throw UnimplementedError();
+      {required String collectionId, required String entryId}) async {
+    final entries = await _openEntriesBox();
+    final entryList = await entries.get(collectionId);
+
+    if (entryList == null) throw CollectionNotFoundException();
+
+    if (!entryList.containsKey(entryId)) throw EntryNotFoundException();
+
+    final entry = entryList[entryId].cast<String, dynamic>();
+    return ToDoEntryModel.fromJson(entry);
   }
 
   @override
