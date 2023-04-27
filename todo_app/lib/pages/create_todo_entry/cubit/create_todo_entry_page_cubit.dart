31c52d3a17bd83d628c981495c662eb6e2586bcc
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 11:17:41 -0300

    fix(CreateTodoEntryPageCubit): add collection id

--- todo_app/lib/pages/create_todo_entry/cubit/create_todo_entry_page_cubit.dart
+++ todo_app/lib/pages/create_todo_entry/cubit/create_todo_entry_page_cubit.dart
@@ -1,8 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_app/domain/entities/unique_id_entity.dart';
 
 part 'create_todo_entry_page_state.dart';
 
 class CreateTodoEntryPageCubit extends Cubit<CreateTodoEntryPageState> {
-  CreateTodoEntryPageCubit() : super(const CreateTodoEntryPageState());
+  final CollectionId collectionId;
+
+  CreateTodoEntryPageCubit({required this.collectionId})
+      : super(const CreateTodoEntryPageState());
 }
