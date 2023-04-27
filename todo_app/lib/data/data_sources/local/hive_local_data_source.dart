0b584225987a011faa0e7f359d21c398658f7628
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-27 17:03:37 -0300

    feat(HiveLocalDataSouce): boxes definitions

--- todo_app/lib/data/data_sources/local/hive_local_data_source.dart
+++ todo_app/lib/data/data_sources/local/hive_local_data_source.dart
@@ -1,8 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:todo_app/data/data_sources/interfaces/todo_local_data_source_interface.dart';
 import 'package:todo_app/data/data_sources/models/todo_entry_model.dart';
 import 'package:todo_app/data/data_sources/models/todo_collection_model.dart';
 
+const String collectionBox = 'collections';
+const String entriesBox = 'entries';
+
 class HiveLocalDataSource implements ToDoLocalDataSourceInterface {
+  late BoxCollection todoCollections;
+
+  bool isInitialize = false;
+
+  Future<void> init() async {
+    if (!isInitialize) {
+      todoCollections = await BoxCollection.open(
+          'todo', {collectionBox, entriesBox},
+          path: "./");
+      isInitialize = true;
+    } else {
+      debugPrint('HiveLocalDataSource is already initialized');
+    }
+  }
+
   @override
   Future<bool> createToDoCollection({required ToDoCollectionModel collection}) {
     // TODO: implement createToDoCollection
