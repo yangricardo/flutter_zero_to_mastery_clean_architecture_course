6f969c35835f214a05874d13fb296702e933a767
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-28 16:19:39 -0300

    build(ApiServiceInterface):  flutter pub run build_runner build

---
+++ jwt_chopper_isar_login_stacked_app/lib/services/api_service.chopper.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_service.dart';
+
+// **************************************************************************
+// ChopperGenerator
+// **************************************************************************
+
+// ignore_for_file: always_put_control_body_on_new_line, always_specify_types, prefer_const_declarations, unnecessary_brace_in_string_interps
+class _$ApiServiceInterface extends ApiServiceInterface {
+  _$ApiServiceInterface([ChopperClient? client]) {
+    if (client == null) return;
+    this.client = client;
+  }
+
+  @override
+  final definitionType = ApiServiceInterface;
+}
