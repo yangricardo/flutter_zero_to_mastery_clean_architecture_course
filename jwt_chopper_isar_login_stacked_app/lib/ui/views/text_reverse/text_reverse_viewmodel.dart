f66b1e553e5ec075b8f10e0966a1ddc5cdbd37d2
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-28 15:44:49 -0300

    feat(TextReverseViewModel): get reversedText

--- jwt_chopper_isar_login_stacked_app/lib/ui/views/text_reverse/text_reverse_viewmodel.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/text_reverse/text_reverse_viewmodel.dart
@@ -1,3 +1,7 @@
+import 'package:jwt_chopper_isar_login_stacked_app/ui/views/text_reverse/text_reverse_view.form.dart';
 import 'package:stacked/stacked.dart';
 
-class TextReverseViewModel extends FormViewModel {}
+class TextReverseViewModel extends FormViewModel {
+  String get reversedText =>
+      hasReverseInput ? reverseInputValue!.split('').reversed.join('') : '----';
+}
