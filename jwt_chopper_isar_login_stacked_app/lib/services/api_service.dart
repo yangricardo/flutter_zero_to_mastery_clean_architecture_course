9ddd538a83917e52cce0dae6c2d2f5e756b7260d
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-28 16:32:09 -0300

    feat(ApiService):  default chopper client instance

--- jwt_chopper_isar_login_stacked_app/lib/services/api_service.dart
+++ jwt_chopper_isar_login_stacked_app/lib/services/api_service.dart
@@ -5,4 +5,14 @@ part "api_service.chopper.dart";
 @ChopperApi(baseUrl: '/api')
 abstract class ApiServiceInterface extends ChopperService {}
 
-class ApiService extends _$ApiServiceInterface {}
+class ApiService extends _$ApiServiceInterface {
+  static ChopperClient chopper = ChopperClient(
+    baseUrl: Uri.parse('http://localhost:3000'),
+    services: [
+      ApiService(),
+    ],
+    converter: const JsonConverter(),
+  );
+
+  ApiService() : super(ApiService.chopper);
+}
