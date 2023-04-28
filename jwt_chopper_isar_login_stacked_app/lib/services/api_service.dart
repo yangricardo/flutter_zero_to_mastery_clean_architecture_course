6b446b393210be10b69118051c15a5984d8eb827
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-04-28 16:19:17 -0300

    feat(ApiServiceInterface):  chopper service implementation

--- jwt_chopper_isar_login_stacked_app/lib/services/api_service.dart
+++ jwt_chopper_isar_login_stacked_app/lib/services/api_service.dart
@@ -1,1 +1,8 @@
+import 'package:chopper/chopper.dart';
+
+part "api_service.chopper.dart";
+
+@ChopperApi(baseUrl: '/api')
+abstract class ApiServiceInterface extends ChopperService {}
+
 class ApiService {}
