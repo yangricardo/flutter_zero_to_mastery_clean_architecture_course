616e0c6fb86c89a435fdb040d66da5f04febef48
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2023-05-02 15:57:16 -0300

    feat: users view model

--- jwt_chopper_isar_login_stacked_app/lib/ui/views/users/users_view.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/users/users_view.dart
@@ -13,10 +13,75 @@ class UsersView extends StackedView<UsersViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-      ),
+      backgroundColor: Colors.grey[900],
+      body: viewModel.isBusy
+          ? Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(Colors.purple[200]),
+                  ),
+                  const Text(
+                    'Loading Users',
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+            )
+          : !viewModel.hasError
+              ? ListView.separated(
+                  padding: const EdgeInsets.only(top: 55),
+                  separatorBuilder: (context, index) => const SizedBox(
+                        height: 20,
+                      ),
+                  itemCount: viewModel.data?.length ?? 0,
+                  itemBuilder: (context, index) => Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 25),
+                        decoration: BoxDecoration(
+                            color: Colors.purple[100],
+                            borderRadius: BorderRadius.circular(5)),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 25, vertical: 20),
+                        alignment: Alignment.centerLeft,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Expanded(
+                              child: Column(
+                                children: <Widget>[
+                                  Text(
+                                    "${viewModel.data?[index].name}",
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 20),
+                                  Text(
+                                    "${viewModel.data?[index].company}",
+                                  )
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              child: Icon(
+                                Icons.star,
+                                color: Colors.purple[200],
+                              ),
+                            )
+                          ],
+                        ),
+                      ))
+              : Container(
+                  color: Colors.red,
+                  alignment: Alignment.center,
+                  child: Text(
+                    viewModel.error.toString(),
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
     );
   }
 
--- jwt_chopper_isar_login_stacked_app/lib/ui/views/users/users_viewmodel.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/users/users_viewmodel.dart
@@ -1,3 +1,13 @@
+import 'package:jwt_chopper_isar_login_stacked_app/app/app.locator.dart';
+import 'package:jwt_chopper_isar_login_stacked_app/models/api/user.dart';
+import 'package:jwt_chopper_isar_login_stacked_app/services/api_service.dart';
 import 'package:stacked/stacked.dart';
 
-class UsersViewModel extends BaseViewModel {}
+class UsersViewModel extends FutureViewModel<List<User>> {
+  final _apiService = locator<ApiService>();
+
+  @override
+  Future<List<User>> futureToRun() {
+    return _apiService.getUsers();
+  }
+}
