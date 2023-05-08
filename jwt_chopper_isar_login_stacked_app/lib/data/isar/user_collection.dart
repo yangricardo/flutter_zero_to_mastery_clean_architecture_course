626261a1021b014738ec30acb00702b4435b2bd6
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-08 13:39:54 -0300

    feat: user collection

---
+++ jwt_chopper_isar_login_stacked_app/lib/data/isar/user_collection.dart
@@ -0,0 +1,13 @@
+import 'package:isar/isar.dart';
+
+part 'user_collection.g.dart';
+
+@collection
+class User {
+  Id id = Isar
+      .autoIncrement; // você também pode attribuir id = null para incrementar automaticamente
+
+  String? name;
+
+  int? age;
+}
