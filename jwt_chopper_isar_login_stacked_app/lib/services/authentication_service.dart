b27d815e95de7924cecf93470096163be2c430cd
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-08 13:10:08 -0300

    feat(AuthenticationService): login and logout

--- jwt_chopper_isar_login_stacked_app/lib/services/authentication_service.dart
+++ jwt_chopper_isar_login_stacked_app/lib/services/authentication_service.dart
@@ -1,5 +1,28 @@
-class AuthenticationService {
+import 'dart:math';
+import 'package:jwt_chopper_isar_login_stacked_app/app/app.locator.dart';
+import 'package:jwt_chopper_isar_login_stacked_app/app/app.router.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class AuthenticationService with ListenableServiceMixin {
+  final _navigationService = locator<NavigationService>();
+  bool loggedIn = false;
+
   bool userLoggedIn() {
-    return true;
+    return loggedIn;
+  }
+
+  void login() {
+    loggedIn = true;
+    final random = Random();
+    _navigationService.clearStackAndShow(Routes.homeView,
+        arguments: HomeViewArguments(startingIndex: random.nextInt(1000)));
+    notifyListeners();
+  }
+
+  void logout() {
+    loggedIn = false;
+    _navigationService.clearStackAndShow(Routes.loginView);
+    notifyListeners();
   }
 }
