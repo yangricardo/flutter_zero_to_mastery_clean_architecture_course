e070fcc0b492922686c1e83d965e5de0c27c8113
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-08 14:16:45 -0300

    build(SignUpView): stacked generate

---
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/sign_up/sign_up_view.form.dart
@@ -0,0 +1,229 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// StackedFormGenerator
+// **************************************************************************
+
+// ignore_for_file: public_member_api_docs, constant_identifier_names, non_constant_identifier_names,unnecessary_this
+
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+const String NameValueKey = 'name';
+const String EmailValueKey = 'email';
+const String PasswordValueKey = 'password';
+
+final Map<String, TextEditingController> _SignUpViewTextEditingControllers = {};
+
+final Map<String, FocusNode> _SignUpViewFocusNodes = {};
+
+final Map<String, String? Function(String?)?> _SignUpViewTextValidations = {
+  NameValueKey: null,
+  EmailValueKey: null,
+  PasswordValueKey: null,
+};
+
+mixin $SignUpView {
+  TextEditingController get nameController =>
+      _getFormTextEditingController(NameValueKey);
+  TextEditingController get emailController =>
+      _getFormTextEditingController(EmailValueKey);
+  TextEditingController get passwordController =>
+      _getFormTextEditingController(PasswordValueKey);
+  FocusNode get nameFocusNode => _getFormFocusNode(NameValueKey);
+  FocusNode get emailFocusNode => _getFormFocusNode(EmailValueKey);
+  FocusNode get passwordFocusNode => _getFormFocusNode(PasswordValueKey);
+
+  TextEditingController _getFormTextEditingController(
+    String key, {
+    String? initialValue,
+  }) {
+    if (_SignUpViewTextEditingControllers.containsKey(key)) {
+      return _SignUpViewTextEditingControllers[key]!;
+    }
+
+    _SignUpViewTextEditingControllers[key] =
+        TextEditingController(text: initialValue);
+    return _SignUpViewTextEditingControllers[key]!;
+  }
+
+  FocusNode _getFormFocusNode(String key) {
+    if (_SignUpViewFocusNodes.containsKey(key)) {
+      return _SignUpViewFocusNodes[key]!;
+    }
+    _SignUpViewFocusNodes[key] = FocusNode();
+    return _SignUpViewFocusNodes[key]!;
+  }
+
+  /// Registers a listener on every generated controller that calls [model.setData()]
+  /// with the latest textController values
+  void syncFormWithViewModel(FormViewModel model) {
+    nameController.addListener(() => _updateFormData(model));
+    emailController.addListener(() => _updateFormData(model));
+    passwordController.addListener(() => _updateFormData(model));
+  }
+
+  /// Registers a listener on every generated controller that calls [model.setData()]
+  /// with the latest textController values
+  @Deprecated(
+    'Use syncFormWithViewModel instead.'
+    'This feature was deprecated after 3.1.0.',
+  )
+  void listenToFormUpdated(FormViewModel model) {
+    nameController.addListener(() => _updateFormData(model));
+    emailController.addListener(() => _updateFormData(model));
+    passwordController.addListener(() => _updateFormData(model));
+  }
+
+  static const bool _autoTextFieldValidation = true;
+  bool validateFormFields(FormViewModel model) {
+    _updateFormData(model, forceValidate: true);
+    return model.isFormValid;
+  }
+
+  /// Updates the formData on the FormViewModel
+  void _updateFormData(FormViewModel model, {bool forceValidate = false}) {
+    model.setData(
+      model.formValueMap
+        ..addAll({
+          NameValueKey: nameController.text,
+          EmailValueKey: emailController.text,
+          PasswordValueKey: passwordController.text,
+        }),
+    );
+
+    if (_autoTextFieldValidation || forceValidate) {
+      updateValidationData(model);
+    }
+  }
+
+  /// Calls dispose on all the generated controllers and focus nodes
+  void disposeForm() {
+    // The dispose function for a TextEditingController sets all listeners to null
+
+    for (var controller in _SignUpViewTextEditingControllers.values) {
+      controller.dispose();
+    }
+    for (var focusNode in _SignUpViewFocusNodes.values) {
+      focusNode.dispose();
+    }
+
+    _SignUpViewTextEditingControllers.clear();
+    _SignUpViewFocusNodes.clear();
+  }
+}
+
+extension ValueProperties on FormViewModel {
+  bool get isFormValid =>
+      this.fieldsValidationMessages.values.every((element) => element == null);
+  String? get nameValue => this.formValueMap[NameValueKey] as String?;
+  String? get emailValue => this.formValueMap[EmailValueKey] as String?;
+  String? get passwordValue => this.formValueMap[PasswordValueKey] as String?;
+
+  set nameValue(String? value) {
+    this.setData(
+      this.formValueMap
+        ..addAll({
+          NameValueKey: value,
+        }),
+    );
+
+    if (_SignUpViewTextEditingControllers.containsKey(NameValueKey)) {
+      _SignUpViewTextEditingControllers[NameValueKey]?.text = value ?? '';
+    }
+  }
+
+  set emailValue(String? value) {
+    this.setData(
+      this.formValueMap
+        ..addAll({
+          EmailValueKey: value,
+        }),
+    );
+
+    if (_SignUpViewTextEditingControllers.containsKey(EmailValueKey)) {
+      _SignUpViewTextEditingControllers[EmailValueKey]?.text = value ?? '';
+    }
+  }
+
+  set passwordValue(String? value) {
+    this.setData(
+      this.formValueMap
+        ..addAll({
+          PasswordValueKey: value,
+        }),
+    );
+
+    if (_SignUpViewTextEditingControllers.containsKey(PasswordValueKey)) {
+      _SignUpViewTextEditingControllers[PasswordValueKey]?.text = value ?? '';
+    }
+  }
+
+  bool get hasName =>
+      this.formValueMap.containsKey(NameValueKey) &&
+      (nameValue?.isNotEmpty ?? false);
+  bool get hasEmail =>
+      this.formValueMap.containsKey(EmailValueKey) &&
+      (emailValue?.isNotEmpty ?? false);
+  bool get hasPassword =>
+      this.formValueMap.containsKey(PasswordValueKey) &&
+      (passwordValue?.isNotEmpty ?? false);
+
+  bool get hasNameValidationMessage =>
+      this.fieldsValidationMessages[NameValueKey]?.isNotEmpty ?? false;
+  bool get hasEmailValidationMessage =>
+      this.fieldsValidationMessages[EmailValueKey]?.isNotEmpty ?? false;
+  bool get hasPasswordValidationMessage =>
+      this.fieldsValidationMessages[PasswordValueKey]?.isNotEmpty ?? false;
+
+  String? get nameValidationMessage =>
+      this.fieldsValidationMessages[NameValueKey];
+  String? get emailValidationMessage =>
+      this.fieldsValidationMessages[EmailValueKey];
+  String? get passwordValidationMessage =>
+      this.fieldsValidationMessages[PasswordValueKey];
+}
+
+extension Methods on FormViewModel {
+  setNameValidationMessage(String? validationMessage) =>
+      this.fieldsValidationMessages[NameValueKey] = validationMessage;
+  setEmailValidationMessage(String? validationMessage) =>
+      this.fieldsValidationMessages[EmailValueKey] = validationMessage;
+  setPasswordValidationMessage(String? validationMessage) =>
+      this.fieldsValidationMessages[PasswordValueKey] = validationMessage;
+
+  /// Clears text input fields on the Form
+  void clearForm() {
+    nameValue = '';
+    emailValue = '';
+    passwordValue = '';
+  }
+
+  /// Validates text input fields on the Form
+  void validateForm() {
+    this.setValidationMessages({
+      NameValueKey: getValidationMessage(NameValueKey),
+      EmailValueKey: getValidationMessage(EmailValueKey),
+      PasswordValueKey: getValidationMessage(PasswordValueKey),
+    });
+  }
+}
+
+/// Returns the validation message for the given key
+String? getValidationMessage(String key) {
+  final validatorForKey = _SignUpViewTextValidations[key];
+  if (validatorForKey == null) return null;
+
+  String? validationMessageForKey = validatorForKey(
+    _SignUpViewTextEditingControllers[key]!.text,
+  );
+
+  return validationMessageForKey;
+}
+
+/// Updates the fieldsValidationMessages on the FormViewModel
+void updateValidationData(FormViewModel model) => model.setValidationMessages({
+      NameValueKey: getValidationMessage(NameValueKey),
+      EmailValueKey: getValidationMessage(EmailValueKey),
+      PasswordValueKey: getValidationMessage(PasswordValueKey),
+    });
