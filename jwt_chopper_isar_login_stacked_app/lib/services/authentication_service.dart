1620cfa4d31881001db6cb65444756c3fe79c5a7
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-08 15:52:06 -0300

    feat(AuthenticationService): login authentication

--- jwt_chopper_isar_login_stacked_app/lib/services/authentication_service.dart
+++ jwt_chopper_isar_login_stacked_app/lib/services/authentication_service.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:jwt_chopper_isar_login_stacked_app/app/app.dialogs.dart';
 import 'package:jwt_chopper_isar_login_stacked_app/app/app.locator.dart';
 import 'package:jwt_chopper_isar_login_stacked_app/app/app.router.dart';
 import 'package:jwt_chopper_isar_login_stacked_app/data/isar/user_collection.dart';
@@ -9,17 +10,38 @@ import 'package:stacked_services/stacked_services.dart';
 class AuthenticationService with ListenableServiceMixin {
   final _navigationService = locator<NavigationService>();
   final _localDataService = locator<LocalDataService>();
+  final _dialogService = locator<DialogService>();
   bool loggedIn = false;
 
   bool userLoggedIn() {
     return loggedIn;
   }
 
-  void login() {
-    loggedIn = true;
-    final random = Random();
-    _navigationService.clearStackAndShow(Routes.homeView,
-        arguments: HomeViewArguments(startingIndex: random.nextInt(1000)));
+  Future<void> login(String email, String password) async {
+    User? user = await _localDataService.getUserByEmail(email);
+    if (user == null) {
+      loggedIn = false;
+      _dialogService.showCustomDialog(
+        variant: DialogType.infoAlert,
+        title: 'Login Error',
+        description: 'email not found',
+      );
+    } else {
+      if (password != user.password) {
+        loggedIn = false;
+        _dialogService.showCustomDialog(
+          variant: DialogType.infoAlert,
+          title: 'Login Error',
+          description: 'password not match',
+        );
+      } else {
+        loggedIn = true;
+        Random random = Random();
+        _navigationService.clearStackAndShow(Routes.homeView,
+            arguments: HomeViewArguments(
+                startingIndex: user.id ?? random.nextInt(1000)));
+      }
+    }
     notifyListeners();
   }
 
