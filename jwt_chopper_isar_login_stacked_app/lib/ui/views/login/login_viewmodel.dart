039900e45b3f72322178868c94b98890e244400c
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-08 13:11:01 -0300

    refactor(LoginViewModel): use authentication service

--- jwt_chopper_isar_login_stacked_app/lib/ui/views/login/login_viewmodel.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/login/login_viewmodel.dart
@@ -1,13 +1,10 @@
-import 'dart:math';
 import 'package:jwt_chopper_isar_login_stacked_app/app/app.locator.dart';
-import 'package:jwt_chopper_isar_login_stacked_app/app/app.router.dart';
+import 'package:jwt_chopper_isar_login_stacked_app/services/authentication_service.dart';
 import 'package:stacked/stacked.dart';
-import 'package:stacked_services/stacked_services.dart';
 
 class LoginViewModel extends BaseViewModel {
-  final _navigationService = locator<NavigationService>();
+  final _authenticationService = locator<AuthenticationService>();
   void login() {
-    final random = Random();
-    _navigationService.navigateToHomeView(startingIndex: random.nextInt(1000));
+    _authenticationService.login();
   }
 }
