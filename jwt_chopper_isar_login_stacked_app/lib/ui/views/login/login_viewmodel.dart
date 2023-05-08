9c601884b5ab258e91b63b293e806c74141de3d9
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-08 15:50:54 -0300

    refactor(LoginViewModel): login with email and password

--- jwt_chopper_isar_login_stacked_app/lib/ui/views/login/login_viewmodel.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/login/login_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:jwt_chopper_isar_login_stacked_app/app/app.locator.dart';
 import 'package:jwt_chopper_isar_login_stacked_app/app/app.router.dart';
 import 'package:jwt_chopper_isar_login_stacked_app/services/authentication_service.dart';
+import 'package:jwt_chopper_isar_login_stacked_app/ui/views/login/login_view.form.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -8,7 +9,10 @@ class LoginViewModel extends FormViewModel {
   final _authenticationService = locator<AuthenticationService>();
   final _navigationService = locator<NavigationService>();
   void login() {
-    _authenticationService.login();
+    _authenticationService.login(
+      emailValue ?? "",
+      passwordValue ?? "",
+    );
   }
 
   void goSignUp() {
