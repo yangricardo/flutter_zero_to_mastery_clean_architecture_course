161c742073c0b998943460579f24d62b4db785e0
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-16 15:08:08 -0300

    refactor(web3service):  build prefixed message

--- jwt_chopper_isar_login_stacked_app/lib/services/web3_service.dart
+++ jwt_chopper_isar_login_stacked_app/lib/services/web3_service.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:stacked/stacked.dart';
 import 'package:web3dart/crypto.dart';
 import 'dart:math'; //used for the random number generator
 import 'package:web3dart/web3dart.dart';
+
+import 'package:web3dart/src/utils/typed_data.dart';
 
 class Web3Service with ListenableServiceMixin {
   static const _messagePrefix = '\u0019Ethereum Signed Message:\n';
@@ -29,5 +32,17 @@ class Web3Service with ListenableServiceMixin {
     final signature = privateKey.signPersonalMessageToUint8List(message);
     print("signature: ${bytesToHex(signature, include0x: true)}");
     return signature;
+  }
+
+  Uint8List buildPrefixedMessage(String message) {
+    final Uint8List payload = strToBytes(message);
+    const _messagePrefix = '\u0019Ethereum Signed Message:\n';
+    final prefix = _messagePrefix + payload.length.toString();
+    final prefixBytes = ascii.encode(prefix);
+
+    // will be a Uint8List, see the documentation of Uint8List.+
+    final prefixedMessage = uint8ListFromList(prefixBytes + payload);
+    print("prefixedMessage: ${bytesToHex(prefixedMessage, include0x: true)}");
+    return prefixedMessage;
   }
 }
