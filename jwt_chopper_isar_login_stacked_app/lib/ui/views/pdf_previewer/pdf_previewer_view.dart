026abe047ce01dbdd53cdddf5b3b03c9b91304fc
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-17 15:16:33 -0300

    feat(PdfPreviewerView): show file path for pdf file from filepicker

--- jwt_chopper_isar_login_stacked_app/lib/ui/views/pdf_previewer/pdf_previewer_view.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/pdf_previewer/pdf_previewer_view.dart
@@ -16,12 +16,17 @@ class PdfPreviewerView extends StackedView<PdfPreviewerViewModel> {
       appBar: AppBar(title: const Text('PDF Preview'), actions: [
         IconButton(
           icon: const Icon(Icons.file_open),
-          onPressed: () => {},
+          onPressed: viewModel.pickFile,
         ),
       ]),
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Container(
         padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+        child: Expanded(
+          child: Center(
+            child: Text(viewModel.pdfFilePath ?? 'No PDF file selected'),
+          ),
+        ),
       ),
     );
   }
--- jwt_chopper_isar_login_stacked_app/lib/ui/views/pdf_previewer/pdf_previewer_viewmodel.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/pdf_previewer/pdf_previewer_viewmodel.dart
@@ -1,3 +1,29 @@
+import 'package:jwt_chopper_isar_login_stacked_app/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class PdfPreviewerViewModel extends BaseViewModel {}
+class PdfPreviewerViewModel extends BaseViewModel {
+  final _dialogService = locator<DialogService>();
+  String? pdfFilePath;
+
+  void pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['pdf'],
+        allowMultiple: false);
+    if (result != null) {
+      pdfFilePath = result.files.single.path;
+      await _dialogService.showDialog(
+        title: 'File found!',
+        description: '$pdfFilePath',
+      );
+    } else {
+      await _dialogService.showDialog(
+        title: 'File not found!',
+        description: 'Please try again.',
+      );
+    }
+    notifyListeners();
+  }
+}
