14dd713cf597e254504f66af2f5d7862185a7191
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-17 12:54:20 -0300

    feat(QrCodeScannerViewModel): show dialog for qr code content

--- jwt_chopper_isar_login_stacked_app/lib/ui/views/qr_code_scanner/qr_code_scanner_view.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/qr_code_scanner/qr_code_scanner_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:stacked/stacked.dart';
 
 import 'qr_code_scanner_viewmodel.dart';
@@ -13,9 +14,14 @@ class QrCodeScannerView extends StackedView<QrCodeScannerViewModel> {
     Widget? child,
   ) {
     return Scaffold(
+      appBar: AppBar(title: const Text('QR Code Scanner')),
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Container(
         padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+        child: MobileScanner(
+          // fit: BoxFit.contain,
+          onDetect: viewModel.onDetect,
+        ),
       ),
     );
   }
--- jwt_chopper_isar_login_stacked_app/lib/ui/views/qr_code_scanner/qr_code_scanner_viewmodel.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/qr_code_scanner/qr_code_scanner_viewmodel.dart
@@ -1,3 +1,20 @@
-import 'package:stacked/stacked.dart';
+import 'dart:typed_data';
 
-class QrCodeScannerViewModel extends BaseViewModel {}
+import 'package:jwt_chopper_isar_login_stacked_app/app/app.locator.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class QrCodeScannerViewModel extends BaseViewModel {
+  final _dialogService = locator<DialogService>();
+  void onDetect(BarcodeCapture capture) async {
+    final List<Barcode> barcodes = capture.barcodes;
+    final Uint8List? image = capture.image;
+    for (final barcode in barcodes) {
+      await _dialogService.showDialog(
+        title: 'Barcode found!',
+        description: '${barcode.rawValue}',
+      );
+    }
+  }
+}
