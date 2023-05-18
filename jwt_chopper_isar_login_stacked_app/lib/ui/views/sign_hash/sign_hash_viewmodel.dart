7e2a67cbed6ee5c101453be8b8ceca7422a1c67b
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-18 10:31:16 -0300

    feat(SignHashView): show confirmation dialog

--- jwt_chopper_isar_login_stacked_app/lib/ui/views/sign_hash/sign_hash_viewmodel.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/sign_hash/sign_hash_viewmodel.dart
@@ -2,16 +2,18 @@ import 'package:jwt_chopper_isar_login_stacked_app/app/app.locator.dart';
 import 'package:jwt_chopper_isar_login_stacked_app/services/authentication_service.dart';
 import 'package:jwt_chopper_isar_login_stacked_app/services/web3_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 import 'package:web3dart/crypto.dart';
 
 class SignHashViewModel extends BaseViewModel {
   final _authenticationService = locator<AuthenticationService>();
   final _web3Service = locator<Web3Service>();
+  final _dialogService = locator<DialogService>();
   String? ethereumAddress;
   String? signatureHex;
   bool? isValid;
 
-  void signHash(String hash) {
+  Future<void> signHash(String hash) async {
     signatureHex = null;
     isValid = false;
     notifyListeners();
@@ -24,6 +26,11 @@ class SignHashViewModel extends BaseViewModel {
         hash,
         ethereumAddress!,
       );
+      await _dialogService.showDialog(
+          title: 'Signature',
+          description: 'Signature: $signatureHex\n'
+              'Signature is valid: $isValid\n'
+              'Ethereum Address: $ethereumAddress');
     }
     notifyListeners();
   }
