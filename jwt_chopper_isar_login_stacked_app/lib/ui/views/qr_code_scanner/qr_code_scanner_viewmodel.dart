904bb14e54c8ca09b0db4e022e8050ac837cb355
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-05-18 10:28:34 -0300

    feat(QrCodeScannerModel): show dialog on detect

--- jwt_chopper_isar_login_stacked_app/lib/ui/views/qr_code_scanner/qr_code_scanner_viewmodel.dart
+++ jwt_chopper_isar_login_stacked_app/lib/ui/views/qr_code_scanner/qr_code_scanner_viewmodel.dart
@@ -1,11 +1,19 @@
+import 'package:jwt_chopper_isar_login_stacked_app/app/app.locator.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class QrCodeScannerViewModel extends BaseViewModel {
   String? pdfHash;
+
+  final _dialogService = locator<DialogService>();
   void onDetect(BarcodeCapture capture) async {
     final List<Barcode> barcodes = capture.barcodes;
     pdfHash = barcodes.firstOrNull?.rawValue;
+    await _dialogService.showDialog(
+        title: 'PDF Hash',
+        description: pdfHash ?? 'No hash detected',
+        barrierDismissible: true);
     notifyListeners();
   }
 }
